0ce62d5304f63b10f968b97f012d282f2a289848
GIGOMEDIA <gratitudeowoade200@gmail.com>
2026-05-05 16:42:49 +0100

    again

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -2,21 +2,27 @@ import { Stack } from "expo-router";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 import { StatusBar } from "expo-status-bar";
 
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
+
 export default function RootLayout() {
   return (
-    <SafeAreaProvider>
-      <Stack
-        screenOptions={{
-          headerShown: false,
-          contentStyle: { backgroundColor: "#07090B" },
-        }}
-      />
+    <QueryClientProvider client={queryClient}>
+      <SafeAreaProvider>
+        <Stack
+          screenOptions={{
+            headerShown: false,
+            contentStyle: { backgroundColor: "#07090B" },
+          }}
+        />
 
-      <StatusBar
-        backgroundColor="#07090B"
-        style="light"
-        translucent={false}
-      />
-    </SafeAreaProvider>
+        <StatusBar
+          backgroundColor="#07090B"
+          style="light"
+          translucent={false}
+        />
+      </SafeAreaProvider>
+    </QueryClientProvider>
   );
 }
